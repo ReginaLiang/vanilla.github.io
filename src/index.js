ca57f12ea4dab906eea0733c8fce099e0eadca22
ReginaLiang <53214295+ReginaLiang@users.noreply.github.com>
2019-07-29 17:54:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello PWA!</h1>
-`;
-
 if('serviceWorker' in navigator){
   navigator.serviceWorker.register('service-worker.js')
   .then((reg) => {
